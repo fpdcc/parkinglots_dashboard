e4443575e2bc51cce762f06a8eeafd266d7d0b32
glw <garretlewis@gmail.com>
2019-05-16 11:20:43 -0500

    try out connection to database

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
 import dash
-import json
+from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table_experiments as dt
+import json
+import plotly
+import plotly.graph_objs as go
 import pandas as pd
 import geopandas as gpd
+from sqlalchemy import create_engine
+import yaml
 
 #############
 # ENV SETUP #
 #############
 config = yaml.safe_load(open("config.yaml"))
 
-mapbox_access_token = config['MAPBOX']['TOKEN']
+# postgresql db connection
+CREATEENGINE = config['postgresql']['CREATEENGINE']
+EPSG = '4326'
+
+###################
+# create connection
+###################
+engine = create_engine(CREATEENGINE)
+sql = 'SELECT lot, sq_feet, geom from public.lots_table order by lot;'
+
+gdf = gpd.GeoDataFrame.from_postgis(sql, con=engine, geom_col='geom')
+gdf_4326 = gdf.to_crs(epsg=EPSG)
 
 app = dash.Dash()
 app.scripts.config.serve_locally=True
 
-gdf  = gpd.read_file( './data/parkinglots_simplified_4326.geojson')
+mapbox_access_token = config['MAPBOX']['TOKEN']
 
 app.layout = html.Div( [ dcc.Graph(
 	id = 'map',
@@ -32,7 +49,7 @@ app.layout = html.Div( [ dcc.Graph(
 					sourcetype = 'geojson',
 					type 	   = 'fill',
 					color	   = '#265465',
-					source     = json.loads( gdf.to_json() )
+					source     = json.loads( gdf_4326.to_json() )
 				)],
 				accesstoken = mapbox_access_token,
 				center  = dict(
