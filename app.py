6233146db57dde294b8538c3bcba8fc29c405ee7
glw <garretlewis@gmail.com>
2019-05-16 11:22:37 -0500

    add callbacks for interactivity. still need to update new data-table package

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import dash
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table_experiments as dt
+import dash_table as dt
+#import dash-bootstrap-components
 import json
 import plotly
-import plotly.graph_objs as go
+from plotly import graph_objs as go
+from plotly.graph_objs import *
 import pandas as pd
-import geopandas as gpd
 from sqlalchemy import create_engine
 import yaml
 
@@ -17,51 +18,117 @@ import yaml
 config = yaml.safe_load(open("config.yaml"))
 
 # postgresql db connection
-CREATEENGINE = config['postgresql']['CREATEENGINE']
-EPSG = '4326'
+# CREATEENGINE = config['postgresql']['CREATEENGINE']
 
 ###################
 # create connection
 ###################
-engine = create_engine(CREATEENGINE)
-sql = 'SELECT lot, sq_feet, geom from public.lots_table order by lot;'
+# engine = create_engine(CREATEENGINE)
+# sql = 'SELECT lot, sq_feet, geom from public.lots_table order by lot;'
 
-gdf = gpd.GeoDataFrame.from_postgis(sql, con=engine, geom_col='geom')
-gdf_4326 = gdf.to_crs(epsg=EPSG)
-
-app = dash.Dash()
-app.scripts.config.serve_locally=True
+# gdf = gpd.GeoDataFrame.from_postgis(sql, con=engine, geom_col='geom' )
 
 mapbox_access_token = config['MAPBOX']['TOKEN']
 
-app.layout = html.Div( [ dcc.Graph(
-	id = 'map',
-	figure = dict(
-		data = [dict(
-			type = 'scattermapbox'
-		)],
-		layout = dict(
-			height   = 900,
-			autosize = True,
-			mapbox 	 = dict(
-				layers =[dict(
+df  = pd.read_csv('./data/parkinglots_simplified_centroid_4326.csv')
+site_lat = list(df['lat'])
+site_lon = list(df['lon'])
+locations_name = list(df['lot'])
 
-					sourcetype = 'geojson',
-					type 	   = 'fill',
-					color	   = '#265465',
-					source     = json.loads( gdf_4326.to_json() )
-				)],
-				accesstoken = mapbox_access_token,
-				center  = dict(
-		            lat = 41.808611,
-		            lon = -87.888889
-		        ),
-		        zoom = 9,
-		        style='light'
-			)
-		)
-	)
-)])
+#external_stylesheet = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+#app = dash.Dash(__name__, external_stylesheets=external_stylesheet)
+app = dash.Dash()
+app.scripts.config.serve_locally=True
+
+app.layout = html.Div([
+    html.H4('FPDCC Parking Lots'),
+    html.Div([
+        html.Div([
+            dcc.Graph(
+            	id = 'map',
+            	figure = {
+            		'data': [ go.Scattermapbox(
+                        lat = site_lat,
+            		    lon = site_lon,
+                        mode = 'markers',
+                        marker = {
+                            'size':10,
+                            'color':'#265465',
+                        }
+                    )],
+            		'layout': go.Layout(
+                        hovermode = "closest",
+            			height = 800,
+            			autosize = True,
+            			mapbox = dict(
+            				accesstoken = mapbox_access_token,
+            				center = {'lat' : 41.808611,'lon' : -87.888889},
+            		        zoom = 9,
+            		        style = 'light'
+                        )
+                    )
+            	}
+            )], className = 'six columns'),
+        html.Div([
+            dcc.Graph(
+                id = 'graph-lots'
+            )
+    ], className = 'six columns')
+    ], className = 'row'),
+
+    html.Div([
+        dt.DataTable(
+            id = 'datatable-lots',
+            data=df.to_dict("rows"),
+            #rows = df.to_dict('records'),
+            columns = sorted(df.columns.difference(['geom'])),
+            editable=True,
+            filtering=True,
+            sorting=True,
+            sorting_type="multi",
+            row_selectable="multi",
+            row_deletable=True,
+            selected_rows=[]
+        )
+    ]),
+], className = "container")
+
+@app.callback(
+    Output('datatable-lots', 'selected_rows'),
+    [Input('graph-lots', 'clickData')],
+    [State('datatable-lots', 'selected_rows')])
+def update_selected_rows(clickData, selected_rows):
+    if clickData:
+        for point in clickData['points']:
+            if point['pointNumber'] in selected_rows:
+                selected_rows.remove(point['pointNumber'])
+            else:
+                selected_rows.append(point['pointNumber'])
+    return selected_rows
+
+
+@app.callback(
+    Output('graph-lots', 'figure'),
+    [Input('datatable-lots', 'data'),
+     Input('datatable-lots', 'selected_rows')])
+def update_figure(data, selected_rows):
+    dff = pd.DataFrame(data)
+    data = [go.Bar(
+            x = dff['lot'],
+            y = dff['sqft'],
+            width = 1,
+            name = 'Lot Square Feet'
+            )
+        ]
+    fig = go.Figure(data=data)
+    return fig
+
+
+#app.css.append_css({
+#    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+#})
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
